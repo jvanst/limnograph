3f28b818b3a48333d4753cb59cb843d412ab4c15
James Vansteenkiste <james@jamesvan.ca>
2025-09-13 14:47:11 -0400

    update(select-all): styling and icon

--- app/components/SelectAll.vue
+++ app/components/SelectAll.vue
@@ -2,13 +2,10 @@
   <button
     type="button"
     :disabled="disabled"
-    class="inline-flex items-center gap-x-0.5 rounded-md px-2 py-1 text-sm font-medium ring-1 ring-inset
-      bg-gray-50 ring-gray-500/10 dark:bg-gray-400/10 dark:ring-gray-400/20 hover:bg-gray-100 dark:hover:bg-gray-500/20
-      text-gray-900 dark:text-gray-100
-      disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed dark:disabled:bg-gray-800 dark:disabled:text-gray-600"
+  class="appearance-none rounded-md bg-white py-1.5 pl-3 pr-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:*:bg-gray-800 dark:focus-visible:outline-indigo-500 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed dark:disabled:bg-gray-800 dark:disabled:text-gray-600 inline-flex items-center gap-x-0.5"
   >
-    <span class="mx-1">Add all</span>
-    <PlusIcon
+    <span class="mx-1">All years</span>
+    <ChartBarIcon
       class="pointer-events-none col-start-1 row-start-1 ml-1 size-5 self-center justify-self-end text-gray-500 sm:size-4 dark:text-gray-400"
       aria-hidden="true"
     />
@@ -16,6 +13,6 @@
 </template>
 
 <script setup lang="ts">
-import { PlusIcon } from "@heroicons/vue/16/solid";
+import { ChartBarIcon } from "@heroicons/vue/24/outline";
 defineProps<{ disabled?: boolean }>();
 </script>
