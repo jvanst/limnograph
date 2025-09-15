958ef6a4f2aa9e3f1edd7bce61391088a92318ee
James Vansteenkiste <james@jamesvan.ca>
2025-09-15 13:39:20 -0400

    update: app path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/kawagama/',
     head: {
       title: 'Kawagama Water Levels',
       meta: [
